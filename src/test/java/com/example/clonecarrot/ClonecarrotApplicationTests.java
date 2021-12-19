28e62bf9ce96c17643543be7e7a28e6c6e3cc4af
kslee <kslee@ksleeui-Macmini.local>
2021-12-19 22:38:34 +0900

    hello~ world!!

---
+++ src/test/java/com/example/clonecarrot/ClonecarrotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.clonecarrot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClonecarrotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
